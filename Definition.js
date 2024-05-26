f015f28e520c8ee3a3141f54744e54710cdbaa86
KristiyanAbrashkov <162896853+KristiyanAbrashkov@users.noreply.github.com>
2024-05-26 16:41:19 +0300

    Revert "Add files via upload"

--- Definition.js
+++
@@ -1,7 +0,0 @@
-let email;
-document.getElementById("button1").onclick = function()
-{
-    email = document.getElementById("input").value;
-    document.getElementById("h3").textContent = `Succesfully registered: ${email}`;
-    alert("Thank you for participating");
-}
--- nav_bar.js
+++
@@ -1,6 +0,0 @@
-const Button = document.getElementById('button');
-const Links = document.getElementById('links');
-
-button.addEventListener('click', () => {
-    links.classList.toggle('active');
-});
